cbf7bd7f31d3026567dfd4e97c4f1e0b6ba1c648
LouisePrd <louise.peridy22@gmail.com>
2025-02-22 20:06:55 +0100

    ♻️ Refactor Cards.js

---
+++ src/components/cards.js
@@ -0,0 +1,13 @@
+import "../assets/styles/cards.css";
+
+export default function Cards({ title, children }) {
+    return (
+      <div className="card">
+        <h2 id="cardTitle">{title}</h2>
+        <div className="cardContent">
+          {children}
+        </div>
+      </div>
+    );
+  }
+  
