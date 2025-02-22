635cbd6f38827ae267c0880f902205d21dc68dd0
LouisePrd <louise.peridy22@gmail.com>
2025-02-22 20:09:14 +0100

    ✨ Overview film OK

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Card from "./components/card";
+import Cards from "./components/cards";
 import SearchBar from "./components/searchBar";
 import { useState } from "react";
 import { useData } from "./services/data";
@@ -7,27 +7,64 @@ import { useData } from "./services/data";
 function App() {
   const [searchValue, setSearchValue] = useState("");
   const { data, error, loading } = useData(searchValue);
+  const [launched, setLaunch] = useState(false);
+  const [selectedFilm, setSelectedFilm] = useState(null);
+
   const listFilm = data?.map((item) => (
-    <Card key={item.id} title={item.title}>
-      <p>{item.overview}</p>
-    </Card>
+    <li key={item.id} onClick={() => handleSelectFilm(item)}>
+      <Cards title={item.title}>
+        <p>{item.overview}</p>
+      </Cards>
+    </li>
   ));
+
+  const handleClick = (value) => {
+    setSearchValue(value);
+    setLaunch(true);
+  };
+
+  const handleSelectFilm = (film) => {
+    setSelectedFilm(film);
+  };
+
   return (
     <div className="App">
-      <h1>Movikipedia</h1>
-      <div className="App-left">
-        <SearchBar onSearch={setSearchValue} />
-        {loading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error: {error.message}</p>
-        ) : (
-          listFilm
-        )}
+      <div className="AppLeft">
+        <h1>Movikipedia</h1>
+        <SearchBar onSearch={handleClick} />
 
-        {!loading && data.length === 0 && <p>No film found</p>}
+        <div className="cards">
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>Error: {error.message}</p>
+          ) : (
+            listFilm
+          )}
+        </div>
+
+        {!loading && launched && data.length === 0 && <p>No film found</p>}
       </div>
-      <div className="App-right"></div>
+      <div className="AppRight">
+        {selectedFilm && (
+          <div className="filmDetails">
+            <h2>{selectedFilm.title}</h2>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${selectedFilm.poster_path}`}
+              alt={selectedFilm.title}
+            />
+            <div className="text">
+              <p>Original title: {selectedFilm.original_title}</p>
+              <p>Original language: {selectedFilm.original_language}</p>
+              <p>Popularity: {selectedFilm.popularity}</p>
+              <p>{selectedFilm.overview}</p>
+              <p>Release date: {selectedFilm.release_date}</p>
+              <p>Rating: {selectedFilm.vote_average}</p>
+              <p>Vote count: {selectedFilm.vote_count}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
