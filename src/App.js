83ef66e9b82890defa99e7f9b6e73c93834327d8
LouisePrd <louise.peridy22@gmail.com>
2025-02-22 18:05:04 +0100

    ✨ Search OK

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './assets/images/logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/card";
+import SearchBar from "./components/searchBar";
+import { useState } from "react";
+import { useData } from "./services/data";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const { data, error, loading } = useData(searchValue);
+  const listFilm = data?.map((item) => (
+    <Card key={item.id} title={item.title}>
+      <p>{item.overview}</p>
+    </Card>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movikipedia</h1>
+      <div className="App-left">
+        <SearchBar onSearch={setSearchValue} />
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : (
+          listFilm
+        )}
+
+        {!loading && data.length === 0 && <p>No film found</p>}
+      </div>
+      <div className="App-right"></div>
     </div>
   );
 }
